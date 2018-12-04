879bc6d2c5f346e2b3f73427055c88c3982727bb
lukefrancis5 <lukebrandonfrancis@gmail.com>
2018-12-04 16:32:25 -0500

    oddOReven

---
+++ iseven.py
@@ -0,0 +1,12 @@
+
+
+def odd_or_even(number):
+    if number % 2 == 0:   
+        print('Number is even')
+    elif number % 2 == 1:
+        print('Number is odd')
+    else:
+        ('Invalid Input')
+
+
+print(odd_or_even(4))
