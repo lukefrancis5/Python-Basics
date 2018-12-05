cde1ec7efacd98e764c754f023113989af8c2d93
lukefrancis5 <lukebrandonfrancis@gmail.com>
2018-12-05 18:42:49 -0500

    Function

---
+++ function.py
@@ -0,0 +1,21 @@
+
+def add(x,y):
+    return x + y
+
+def sub(x,y):
+    return x - y
+
+def mult(x,y):
+    return x * y
+
+def div(x,y):
+    return x / y
+
+
+print(add(10,5))
+print(sub(7,4))
+print(mult(5,5))
+print(div(20,4))
+
+
+
