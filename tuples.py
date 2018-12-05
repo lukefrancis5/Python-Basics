4eafbe91dab6b8582a67d8d85569785f39181c28
lukefrancis5 <lukebrandonfrancis@gmail.com>
2018-12-05 18:35:27 -0500

    Tuples

---
+++ tuples.py
@@ -0,0 +1,11 @@
+
+
+presidents = [
+    ('George Washington', 1789),
+    ('John Adams', 1797),
+    ('Thomas Jefferson', 1801),
+    ('James Madison', 1809)
+]
+
+for year, prez in presidents:
+    print('In {}, {} took office'.format(year,prez))
