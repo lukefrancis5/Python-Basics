ee499f940504aaee4794b0b4e74f5004027645e2
lukefrancis5 <lukebrandonfrancis@gmail.com>
2019-02-06 20:15:15 -0500

    modified iseven

--- iseven.py
+++ iseven.py
@@ -1,12 +1,12 @@
 
 
-def odd_or_even(number):
+def even_or_odd(number):
     if number % 2 == 0:   
-        print('Number is even')
+        print('Number is even!')
     elif number % 2 == 1:
-        print('Number is odd')
+        print('Number is odd!')
     else:
         ('Invalid Input')
 
 
-print(odd_or_even(4))
+print(even_or_odd(4))
